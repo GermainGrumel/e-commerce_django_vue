417316cf50fa0a59e9844fffc3901a07ca8dfb3e
Germain Grumel <germaingrumel@outlook.fr>
2021-07-16 23:58:21 +0200

    pushing vue files

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="router">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <strong>Djackets</strong>
+        </router-link>
+        <a
+          href="#"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div
+        class="navbar-menu"
+        id="navbar-menu"
+        v-bind:class="{ 'is-active': showMobileMenu }"
+      >
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">
+            <strong>Summer</strong>
+          </router-link>
+          <router-link to="/winter" class="navbar-item">
+            <strong>Winter</strong>
+          </router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light"
+                >Log in</router-link
+              >
+              <router-link to="/log-in" class="button is-success">
+                <span class="icon">
+                  <i class="fas fa-shopping-cart"></i>
+                </span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="section">
+      <router-view />
+    </section>
+    <footer class="footer">
+      <p class="has-text-centered">from France</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
